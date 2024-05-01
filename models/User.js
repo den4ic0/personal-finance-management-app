8a7864c18a809af8bb96f2c3a820ff03c4380e2a
den4ic0 <163349334+den4ic0@users.noreply.github.com>
2024-05-01 21:08:59 +0300

    cleaning code

--- models/User.js
+++ models/User.js
@@ -1,53 +1,38 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-require('dotenv').config(); // Ensure environment variables are read
+require('dotenv').config();
 
-// Define User Schema
 const userSchema = new mongoose.Schema({
-  // Username must be unique and is required
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
-  // Email must be unique and is required
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
-  // Password hash is stored, not the plaintext password
   passwordHash: {
     type: String,
-    required: true
+    required: true,
   },
-  // Reference to transaction IDs, establishing a relation with the 'Transaction' model
   transactionIDs: [{
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Transaction'
+    ref: 'Transaction',
   }],
-}, { timestamps: true }); // Enable automatic creation of createdAt and updatedAt timestamps
+}, {
+  timestamps: true,
+});
 
-/**
- * Hashes the user's password.
- *
- * @param {string} password - The plaintext password to hash.
- */
 userSchema.methods.setPassword = async function(password) {
-  const saltRounds = parseInt(process.env.SALT_ROUNDS) || 10;  // Get the number of salt rounds from .env or use default
+  const saltRounds = parseInt(process.env.SALT_ROUNDS, 10) || 10;
   this.passwordHash = await bcrypt.hash(password, saltRounds);
 }
 
-/**
- * Validates a given password against the stored hash.
- *
- * @param {string} password - The plaintext password to validate.
- * @returns {Promise<boolean>} - A promise that resolves with a boolean indicating if the password is valid.
- */
 userSchema.methods.validatePassword = async function(password) {
-  return await bcrypt.compare(password, this.passwordHash);
+  return bcrypt.compare(password, this.passwordHash);
 }
 
-// Create and export the User model
 const User = mongoose.model('User', userSchema);
 module.exports = User;
