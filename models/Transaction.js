89afd5aee20820fa30abe16b1659098a379b459b
den4ic0 <163349334+den4ic0@users.noreply.github.com>
2024-05-04 12:58:01 +0300

    error catching update

--- models/Transaction.js
+++ models/Transaction.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.error('MongoDB connection error:', err.message));
 
 const TransactionSchema = new mongoose.Schema({
   amount: {
@@ -25,32 +25,49 @@ const TransactionSchema = new mongoose.Schema({
   }
 });
 
-TransactionSchema.statics.createTransaction = async function(transactionDetails) {
+TransactionSchema.statics.createTransaction = async function (transactionDetails) {
   try {
     const newTransaction = await this.create(transactionDetails);
     return newTransaction;
-  } catch(error) {
-    throw error;
+  } catch (error) {
+    handleError(error);
   }
 };
 
-TransactionSchema.statics.removeTransactionById = async function(transactionId) {
+TransactionSchema.statics.removeTransactionById = async function (transactionId) {
   try {
     const deletionResult = await this.deleteOne({ _id: transactionId });
+    if (deletionResult.deletedCount === 0) {
+      throw new Error(`Transaction with ID ${transactionId} not found.`);
+    }
     return deletionResult;
-  } catch(error) {
-    throw error;
+  } catch (error) {
+    handleError(error);
   }
 };
 
-TransactionSchema.statics.modifyTransaction = async function(transactionId, updatedDetails) {
+TransactionSchema.statics.modifyTransaction = async function (transactionId, updatedDetails) {
   try {
     const updatedTransaction = await this.findByIdAndUpdate(transactionId, updatedDetails, { new: true });
+    if (!updatedTransaction) {
+      throw new Error(`Transaction with ID ${transactionId} not found or could not be updated.`);
+    }
     return updatedTransaction;
-  } catch(error) {
-    throw error;
+  } catch (error) {
+    handleError(error);
   }
 };
+
+function handleError(error) {
+  if (error.name === 'ValidationError') {
+    const messages = Object.values(error.errors).map(val => val.message);
+    throw new Error(`Validation Error: ${messages.join('. ')}`);
+  } else if (error.name === 'CastError') {
+    throw new Error(`Invalid ID format: ${error.value}`);
+  } else {
+    throw new Error(`An error occurred: ${error.message}`);
+  }
+}
 
 const TransactionModel = mongoose.model('Transaction', TransactionSchema);
 
