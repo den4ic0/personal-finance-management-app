bd96c108aebb3d29b8b0f800992f60f8eb38a89a
den4ic0 <163349334+den4ic0@users.noreply.github.com>
2024-05-07 07:52:47 +0300

    improve variables

--- server.js
+++ server.js
@@ -14,71 +14,71 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 app.use(bodyParser.json());
 
-function splitAuthorizationHeader(header) {
+function extractTokenFromHeader(header) {
   return header.split(' ')[1];
 }
 
-function authenticateToken(req, res, next) {
-  const authorizationHeader = req.headers['authorization'];
-  if (authorizationHeader) {
-    const token = splitAuthorizationHeader(authorizationHeader);
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-      if (err) return res.sendStatus(403);
-      req.user = user;
+function verifyAuthentication(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  if (authHeader) {
+    const token = extractTokenFromHeader(authHeader);
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
+      if (err) return res.sendStatus(403); // Forbidden
+      req.user = decodedUser;
       next();
     });
   } else {
-    res.sendStatus(401);
+    res.sendStatus(401); // Unauthorized
   }
 }
 
 app.post('/register', async (req, res) => {
   try {
-    const encryptedPassword = await bcrypt.hash(req.body.password, 8);
-    await registerUser(req.body.username, encryptedPassword);
+    const hashedPassword = await bcrypt.hash(req.body.password, 8);
+    await saveNewUser(req.body.username, hashedPassword);
     res.status(201).send('User registered successfully');
   } catch (error) {
     res.status(500).send('User registration failed');
   }
 });
 
-async function registerUser(username, encryptedPassword){
-    const newUser = new User({ username, password: encryptedPassword });
-    await newUser.save();
+async function saveNewUser(username, hashedPassword){
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
 }
 
 app.post('/login', async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
     if (!user) return res.status(400).send('User not found');
-    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
-    if (!isPasswordCorrect) return res.status(401).send('Login not authorized');
-    const token = jwt.sign({ user: { _id: user._id } }, process.env.JWT_SECRET, { expiresIn: '2h' });
-    res.json({ token });
+    const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isPasswordMatch) return res.status(401).send('Login not authorized');
+    const authToken = jwt.sign({ user: { _id: user._id } }, process.env.JWT_SECRET, { expiresIn: '2h' });
+    res.json({ token: authToken });
   } catch(error) {
     res.sendStatus(500);
   }
 });
 
-app.post('/transaction', authenticateToken, async (req, res) => {
+app.post('/transaction', verifyAuthentication, async (req, res) => {
   try {
-    const newTransaction = await createTransaction(req.user._id, req.body);
-    res.json({ message: 'Transaction created successfully', transaction: newTransaction });
+    const transaction = await recordNewTransaction(req.user._id, req.body);
+    res.json({ message: 'Transaction created successfully', transaction: transaction });
   } catch (error) {
     res.status(500).send('Failed to save transaction');
   }
 });
 
-async function createTransaction(userId, transactionData) {
-  const newTransaction = new Transaction({ userId, ...transactionData });
-  await newTransaction.save();
-  return newTransaction;
+async function recordNewTransaction(userId, transactionData) {
+  const transaction = new Transaction({ userId, ...transactionData });
+  await transaction.save();
+  return transaction;
 }
 
-app.get('/transactions', authenticateToken, async (req, res) => {
+app.get('/transactions', verifyAuthentication, async (req, res) => {
   try {
-    const userTransactions = await Transaction.find({ userId: req.user._id });
-    res.json(userTransactions);
+    const transactionsOfUser = await Transaction.find({ userId: req.user._id });
+    res.json(transactionsOfUser);
   } catch (error) {
     res.status(500).send('Failed to retrieve transactions');
   }
